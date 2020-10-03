eb48a2aa4e2a1a4a428fee9b5a2ae69b5721fdc8
Berk Hüseyin Gaffaroğlu <berkgaffaroglu@Berks-MacBook-Pro.local>
2020-10-03 03:10:33 +0300

    heroku

--- memorychallange/settings.py
+++ memorychallange/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
